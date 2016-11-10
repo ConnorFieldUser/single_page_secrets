3dc565821034d2af8427543c497ded7d46b8eea8
Dan Dietz <danddietz@gmail.com>
2016-11-10 11:27:29 -0400

    added some files

---
+++ app/static/app/scripts/models/secret.js
@@ -0,0 +1,14 @@
+var Backbone = require('backbone');
+
+var Secret = Backbone.Model.extend({
+
+});
+
+var SecretCollection = Backbone.Collection.extend({
+
+});
+
+module.exports = {
+  Secret: Secret,
+  SecretCollection: SecretCollection
+};
--- app/static/app/scripts/models/user.js
+++ app/static/app/scripts/models/user.js
@@ -4,7 +4,11 @@ var User = Backbone.Model.extend({
 
 },{
   login: function(username, password){
+    var loginUrl = '';
+    $.post(loginUrl, {username: username, password: password}).then(function(result){
+      console.log(result);
 
+    });
   }
 });
 
