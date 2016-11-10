ef1f5f7820c7badaeed59f445c7c35e14101df48
taddeimania <jtaddei@gmail.com>
2016-11-10 10:26:20 -0500

    started api auth stuff

--- spa_project/urls.py
+++ spa_project/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
+from rest_framework.authtoken import views
 
 from app.views import IndexView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', IndexView.as_view(), name="index_view")
+    url(r'^$', IndexView.as_view(), name="index_view"),
+    url(r'^obtain_token/', views.obtain_auth_token, name="obtain_auth_token")
 ]
